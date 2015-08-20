0c4584fea7da41ac50d50c7691538083c6379fdb
Faris Hammoud <farishammoud89@hotmail.com>
2015-08-20 16:28:47 -0700

    Update GameObject.java

--- GameObject.java
+++ GameObject.java
@@ -45,6 +45,16 @@ public abstract class GameObject {
 		this.y = y;
 	}
 	
+	public void setVelocity(int x, int y){
+		diffX = x;
+		diffY = y;
+	}
+	
+	public Point getVelocity(){
+		Point v = new Point(diffX, diffY);
+		return v;
+	}
+	
 	public Point getCenter(){
 		Point center = new Point(x + width / 2, y + height / 2);
 		return center;
@@ -54,53 +64,31 @@ public abstract class GameObject {
 		boolean isOverlap = false;
 		
 		Point otherTopLeft = other.getPos();
-		Point otherTopRight = new Point((int)otherTopLeft.x + other.getWidth(), (int)otherTopLeft.y);
-		Point otherBottomLeft = new Point((int)otherTopLeft.x, (int)otherTopLeft.y + other.getHeight());
-		Point otherBottomRight = new Point((int)otherTopLeft.x + other.getWidth(), (int)otherTopLeft.y + other.getHeight());
-		
+		int otherLeftX = otherTopLeft.x;
+		int otherTopY = otherTopLeft.y;
+		int otherRightX = otherLeftX + other.width;
+		int otherBottomY = otherTopY + other.height;
+
 		//top left corner of this
-		int thisTopLeftX = this.x;
-		int thisTopLeftY = this.y;
-		int thisTopRightX = this.x + this.width;
-		int thisTopRightY = this.y;
-		int thisBottomLeftX = this.x;
-		int thisBottomLeftY = this.y + this.height;
-		int thisBottomRightX = this.x + this.width;
-		int thisBottomRightY = this.y + this.height;
+		int thisLeftX = this.x;
+		int thisTopY = this.y;
+		int thisRightX = this.x + this.width;
+		int thisBottomY = this.y + this.height;
 		
-		if (thisTopLeftX >= otherTopLeft.x && thisTopLeftX <= otherTopRight.x &&
-			thisTopLeftY >= otherTopRight.y && thisTopLeftY <= otherBottomRight.y ||
+		if (thisLeftX >= otherLeftX && thisLeftX <= otherRightX &&
+			thisTopY >= otherTopY && thisTopY <= otherBottomY ||
 			
-			thisTopRightX >= otherTopLeft.x && thisTopRightX <= otherTopRight.x &&
-			thisTopRightY >= otherTopRight.y && thisTopRightY <= otherBottomRight.y ||
+			thisRightX >= otherLeftX && thisRightX <= otherRightX &&
+			thisTopY >= otherTopY&& thisTopY <= otherBottomY ||
 			
-			thisBottomRightX >= otherTopLeft.x && thisBottomRightX <= otherTopRight.x &&
-			thisBottomRightY >= otherTopRight.y && thisBottomRightY <= otherBottomRight.y ||
+			thisRightX >= otherLeftX && thisRightX <= otherRightX &&
+			thisBottomY >= otherTopY && thisBottomY <= otherBottomY ||
 			
-			thisBottomLeftX >= otherTopLeft.x && thisBottomLeftX <= otherTopRight.x &&
-			thisBottomLeftY >= otherTopRight.y && thisBottomLeftY <= otherBottomRight.y)
+			thisLeftX >= otherLeftX && thisLeftX <= otherRightX &&
+			thisBottomY >= otherTopY && thisBottomY <= otherBottomY)
 			{
 				isOverlap = true;
 			}
-		
-		
-		if (isOverlap && this instanceof Ball)
-		{
-			double distance;
-			
-			//collision with another ball
-			if (other instanceof Ball)
-			{
-				
-			}
-			else //collision with rectangle
-			{
-				distance = Math.min(Math.sqrt(Math.pow((otherBottomRight.x - this.getCenter().x), 2) + Math.pow((otherBottomRight.y - this.getCenter().y), 2)),Math.min(Math.sqrt(Math.pow((otherBottomLeft.x - this.getCenter().x), 2) + Math.pow((otherBottomLeft.y - this.getCenter().y), 2)), Math.min(Math.sqrt(Math.pow((otherTopLeft.x - this.getCenter().x), 2) + Math.pow((otherTopLeft.y - this.getCenter().y), 2)), Math.sqrt(Math.pow((otherTopRight.x - this.getCenter().x), 2) + Math.pow((otherTopRight.y - this.getCenter().y), 2)))));
-				System.out.println(distance);
-				if (distance > this.width / 2)
-					isOverlap = false;
-			}
-		}
 		
 		return isOverlap;
 	}
